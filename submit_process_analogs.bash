fb4ffa716ac88432b3b87911c260824cc24137e7
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-09-03 10:16:52 +0100

    new file for testing batching

--- submit_process_analogs.bash
+++ submit_process_analogs.bash
@@ -4,35 +4,38 @@
 #SBATCH --time=500:00
 #SBATCH -o /home/users/benhutch/unseen_functions/logs/sub-process-analogs-%A_%a.out
 #SBATCH -e /home/users/benhutch/unseen_functions/logs/sub-process-analogs-%A_%a.err
-#SBATCH --array=1960-2018
+#SBATCH --array=1960-1965
 
 # Set up the usage messages
-usage="Usage: ${init_month}"
+usage="Usage: sbatch submit_process_analogs.bash"
 
 # Check the number of CLI arguments
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 0 ]; then
     echo "Illegal number of parameters"
     echo $usage
     exit 1
 fi
 
-# Set up the CLI arguments
-init_month=$1
+# Set up a list of months
+months=("11" "12" "1" "2" "3")
 
 module load jaspy
 
 # Set up the process script
 process_script="/home/users/benhutch/unseen_functions/unseen_analogs_functions.py"
 
-# Echo the CLI arguments
-echo "Init year; ${SLURM_ARRAY_TASK_ID}"
-echo "Init month: ${init_month}"
+# loop over the months
+for month in "${months[@]}"; do
+    # Echo the CLI arguments
+    echo "Init year; ${SLURM_ARRAY_TASK_ID}"
+    echo "Init month: ${init_month}"
 
-# Run the script
-#!/bin/bash
-python ${process_script} \
-    --init_year ${SLURM_ARRAY_TASK_ID} \
-    --init_month ${init_month}
+    # Run the script
+    #!/bin/bash
+    python ${process_script} \
+        --init_year ${SLURM_ARRAY_TASK_ID} \
+        --init_month ${init_month}
+done
 
 # End of file
 echo "End of file"
